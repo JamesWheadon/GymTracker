033e87ae3cf191eec7b98ff1f5339faebb22f9e2
JamesWheadon <james.wheadon@yahoo.com>
2023-12-12 21:14:02 +0000

    Select Workout screen

--- app/src/main/java/com/example/gymtracker/ui/GymTrackerApp.kt
+++ app/src/main/java/com/example/gymtracker/ui/GymTrackerApp.kt
@@ -7,10 +7,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.gymtracker.ui.exercise.ExercisesScreen
 import com.example.gymtracker.ui.exercise.ExercisesRoute
+import com.example.gymtracker.ui.exercise.ExercisesScreen
 import com.example.gymtracker.ui.exercise.details.ExerciseDetailsRoute
 import com.example.gymtracker.ui.exercise.details.ExerciseDetailsScreen
+import com.example.gymtracker.ui.navigation.NavigationRoutes.EXERCISE_DETAILS_SCREEN
+import com.example.gymtracker.ui.navigation.NavigationRoutes.WORKOUT_DETAILS_SCREEN
 import com.example.gymtracker.ui.navigation.getHomeNavigationOptionsForRoute
 import com.example.gymtracker.ui.workout.WorkoutsRoute
 import com.example.gymtracker.ui.workout.WorkoutsScreen
@@ -40,13 +42,13 @@ fun GymTrackerApp(
             )
         }
         composable(ExerciseDetailsRoute.route,
-            arguments = listOf(navArgument(ExerciseDetailsRoute.navArgument) { type = NavType.IntType })) {
+            arguments = listOf(navArgument(EXERCISE_DETAILS_SCREEN.navigationArgument) { type = NavType.IntType })) {
             ExerciseDetailsScreen(
                 navController = navController
             )
         }
         composable(WorkoutDetailsRoute.route,
-            arguments = listOf(navArgument(WorkoutDetailsRoute.navArgument) { type = NavType.IntType })) {
+            arguments = listOf(navArgument(WORKOUT_DETAILS_SCREEN.navigationArgument) { type = NavType.IntType })) {
             WorkoutDetailsScreen(
                 navController = navController,
                 exerciseNavigationFunction = { id: Int -> navController.navigate(ExerciseDetailsRoute.getRouteForNavArgument(id)) },
